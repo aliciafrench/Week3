80db0cf772495f67009558e3aca3c6ec2731f5c5
Alicia French <alienor18@yahoo.com>
2014-01-28 00:09:23 -0800

    Finished assignment week3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogAQ::Application.config.secret_key_base = '907653c0e077ad76aefadbe98d7b894573644aa856beac05e7ffbe8e65e5b9b19f29c4b2a16c5bc322c53474e6379f028788eb4642c59c73517dd047d95e03e3'
